b32925eac03365c9e365cbae78f7359f0e9bb17a
bleem <2825916685@qq.com>
2020-10-18 17:56:46 +0800

    完成login

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button type="info" @click="goBack">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      // 清空 token
+      window.sessionStorage.clear()
+      // 返回登陆页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
