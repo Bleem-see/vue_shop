e47de287c5ee811f00010c715c1ee90612c17f1d
bleem <2825916685@qq.com>
2020-11-02 00:05:57 +0800

    完成商品列表的开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import './assets/css/global.css'
 // 导入 treeTable
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 引入axios
 import axios from 'axios'
 // 配置请求根路径
@@ -25,8 +32,26 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+// 全局定义一个过滤器 用于指定日期的格式
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  // padStart(2, '0') 第一个参数用来指定字符串的最小长度，第二个参数是用来补全的字符串。
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 // 全局注册 treeTable
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
